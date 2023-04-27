532fd4356bdb5e24eade05abdc297a48185f9874
AbigailGuz <abyguzman@live.com>
2023-04-27 17:50:22 -0300

    se agrega el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+import { FirebaseStorage } from '@angular/fire/storage';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Abigail_Guzman';
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontend-argeprog',
+    appId: '1:257132482897:web:9f7287ca61976c21a1c370',
+    storageBucket: 'frontend-argeprog.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAjidU6tOJW5cAElOKntA3KUFH_CPmQcaw',
+    authDomain: 'frontend-argeprog.firebaseapp.com',
+    messagingSenderId: '257132482897',
+    measurementId: 'G-DHKM0B6TBZ',
+  },};
