c0e7fb8058e442d35a1974ae038491ba20176fca
AbigailGuz <110138520+AbigailGuz@users.noreply.github.com>
2023-05-14 18:27:18 -0300

    Update persona.service.ts

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -7,7 +7,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-URL = 'http://localhost:8080/personas/'; 
+URL = 'https://argprog-backend-production.up.railway.app/personas/'; 
 
   constructor(private httpClient: HttpClient) { }
 
